d58543f7c2df6abfc949d9df78c2cb0f4e65fac3
s27800 <s27800@pjwstk.edu.pl>
2025-12-14 23:23:52 +0100

    feat: making predictions

--- src/api/main.py
+++ src/api/main.py
@@ -1,11 +1,45 @@
-from fastapi import FastAPI
+import os
+import pandas as pd
+from typing import Optional
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, Field
+from autogluon.tabular import TabularPredictor
+
+from ..asi_group_15_01.pipelines.data_science.nodes import basic_clean
+
+MODEL_PATH = os.getenv(
+    "MODEL_PATH", "data/06_models/ag_production/ds_sub_fit/sub_fit_ho"
+)
+MODEL_VERSION = "local"
+PREDICTOR: Optional[TabularPredictor] = None
+
 
 app = FastAPI(
     title="Income Prediction API",
     description="API for income prediction (<=50K or >50K) based on demographic data.",
     version="0.1.0",
 )
+
+
+def load_model():
+    """
+    Loads model.
+    """
+
+    if os.path.exists(MODEL_PATH):
+        return TabularPredictor.load(MODEL_PATH)
+    else:
+        return None
+
+
+@app.on_event("startup")
+async def on_startup():
+    """
+    Prepare singleton.
+    """
+
+    global PREDICTOR
+    PREDICTOR = load_model()
 
 
 # Pydantic scheme
@@ -85,4 +119,36 @@ def predict(payload: Features):
     Endpoint for making predictions.
     """
 
-    return {"prediction": 0, "probability": 0.85, "model_version": "local-dev-stub"}
+    if PREDICTOR is None:
+        raise HTTPException(status_code=503, detail="Model did not load.")
+
+    input_data = payload.model_dump(by_alias=True)
+    df = pd.DataFrame([input_data])
+
+    if "income" not in df.columns:
+        df["income"] = "<=50K"
+
+    try:
+        df = basic_clean(df)
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=f"Error in basic_clean: {e}")
+
+    if df.empty:
+        raise HTTPException(status_code=422, detail="Data is not valid.")
+
+    required_features = PREDICTOR.feature_metadata_in.get_features()
+
+    for feature in required_features:
+        if feature not in df.columns:
+            df[feature] = 0
+
+    df = df[required_features]
+
+    y_pred = PREDICTOR.predict(df).iloc[0]
+    y_proba = PREDICTOR.predict_proba(df).iloc[0][1]
+
+    return {
+        "prediction": int(y_pred),
+        "probability": float(y_proba),
+        "model_version": MODEL_VERSION,
+    }
